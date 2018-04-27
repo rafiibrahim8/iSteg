b3484012e713b2b6cfd07547d5d86066beb25b6c
Ibrahim Rafi <30957188+rafiibrahim8@users.noreply.github.com>
2018-04-27 21:09:02 +0600

    Add files via upload

---
+++ Chunker.java
@@ -0,0 +1,140 @@
+/*
+This class chunks the input into specified binary length. 
+Example-1: 
+we have inSize = 8 and outSize = 6 (out<=in) and the input is 0b1101101001
+by get() method we will get 0b011010 and 0b010000 [11 at the beginning will be discarded cause inSize = 8
+
+Example-2:
+we have inSize = 6 and outSize = 8 (out>in) and the input is 0b110010 and 0b011001
+by get() method we will get 0b11001001 [1001 of 2nd input will be added prior to next input]
+*/
+package isteg;
+import java.util.ArrayList;
+
+public class Chunker {
+    private final int inSize,outSize,maxPossible;
+    private ArrayList<Integer> list;
+    private int rem,remSize,currentPos;
+    private boolean wasExtra;
+    
+    public Chunker(int inSize,int outSize){
+        this.outSize = outSize;
+        this.inSize = inSize;
+        this.maxPossible = getMaxPossible(); // get max possible value of given outSize, Example: 0b111 for 3
+        init(); //initialize variables
+    }
+    
+    
+    private int getMaxPossible(){
+        String s="0";
+        for(int i=0;i<outSize;i++)
+            s = s+"1";
+        return Integer.parseInt(s, 2);
+    }
+    
+    private void init() {
+        this.rem=0; //remaining of previous chunk
+        this.remSize=0; // length of rem
+        this.wasExtra=false;
+        this.currentPos = 0;
+        this.list=new ArrayList<>();
+    }
+    
+    public void add(int x){
+        if(wasExtra)
+            list.remove(list.size()-1); // We put extra 0's at the end of previous chunk
+        rem = rem << inSize | x;
+            remSize+=inSize;
+        while(remSize >= outSize){
+                remSize-=outSize; // new remSize for discarding the value in next line
+                list.add((rem>>remSize));
+                rem = rem << (Integer.SIZE-remSize) >> (Integer.SIZE-remSize); 
+        }
+        
+        if(remSize>0 && outSize<inSize){
+            list.add((rem<<(outSize-remSize)));
+            wasExtra = true;
+        }
+        else
+            wasExtra  = false;
+    }
+    
+    public void add(byte[] bytes){
+        int[] arr = new int[bytes.length];
+        for(int i=0;i<bytes.length;i++){
+            arr[i] = bytes[i];
+        }
+        add(arr);
+    }
+    
+    public void add(int[] ints){
+        for(int i=0;i<ints.length;i++){
+            add(ints[i]);
+        }
+    }
+    
+    public void add(byte b){
+        add((int)b);
+    }
+    
+    public boolean hasNext(){
+        return currentPos < list.size();
+    }
+    
+    public int get(){
+        int r = list.get(currentPos++);
+        return r & maxPossible; //Only Taking Chunk Sized data
+    }
+    
+    public String getBinStr(){
+        String str = Integer.toBinaryString(get());
+        while(str.length()<outSize){
+            str= "0" + str;
+        }
+        return str;
+    }
+    
+    public void setCurrent(int c){
+        currentPos = c;
+    }
+    
+    public int getSize(){
+        return list.size();
+    }
+    
+    public void clearRim(){
+        this.rem = this.remSize = 0;
+    }
+    
+    public void remove(int index){
+        list.remove(index);
+    }
+    
+    public void removeLast(){
+        list.remove(list.size()-1);
+    }
+    
+    public int[] getChunkedIntArray(){
+        int tempCur = currentPos;
+        int[] arr = new int[list.size()];
+        currentPos = 0;
+        for(int i=0;hasNext();i++){
+            arr[i] = get();
+        }
+        currentPos = tempCur;
+        return arr;
+    }
+    
+    public byte[] getChunkedByteArray(){
+        if(outSize>Byte.SIZE)
+            return null;
+        int tempCur = currentPos;
+        byte[] arr = new byte[list.size()];
+        currentPos = 0;
+        for(int i=0;hasNext();i++){
+            arr[i] = (byte)get();
+        }
+        currentPos = tempCur;
+        return arr;
+    }
+}
---
+++ Steg.java
@@ -0,0 +1,291 @@
+package isteg;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+public class Steg {
+    public static final int SUCCESS = 0;
+    public static final int ERR_BITCOUNT = 1;
+    public static final int ERR_FILEREAD = 2;
+    public static final int ERR_FILEWRITE = 3;
+    public static final int ERR_NOSTEG = 4;
+    public static final int ERR_LOWIMGSIZE = 4;
+	private static FileOutputStream fos;
+    
+    public static String[] read(Path topFile){
+        BufferedImage steg;
+        String[] ret = new String[]{"",""};
+        try {
+            steg = ImageIO.read(topFile.toFile());
+        } catch (IOException e) {
+            ret[0]+=Steg.ERR_FILEREAD;
+            return ret;
+        }
+        
+        int rgb0 = steg.getRGB(0,0);
+        int bitCount = (rgb0>>16 & 0b1)+1;
+        int type = rgb0>>8 & 0b1;
+        int enc = rgb0 & 0b1;
+        
+        //Verifying if the image has steganographic data in it by signature iSteg0
+        
+        int bitMaxVal = (bitCount==2)? 0b11:0b1;
+        Chunker sign = new Chunker(bitCount*3,Byte.SIZE);
+        OneDimMaker odm = new OneDimMaker(steg.getWidth(),steg.getHeight());
+        for(int i=2;i<16/bitCount+2;i++){
+            int rgb = steg.getRGB(odm.get(i).x, odm.get(i).y);
+            int value= rgb>>16 & bitMaxVal;
+            value = value<<bitCount | (rgb>>8 & bitMaxVal);
+            value = value<<bitCount | (rgb & bitMaxVal);
+            sign.add(value);
+        }
+        
+        if(! "iSteg0".equals(new String(sign.getChunkedByteArray()))){
+            ret[0]+=Steg.ERR_NOSTEG;
+            return ret;
+        }
+        
+        if(type==0){
+            ret = readFileSteg(steg,bitCount,enc,2+16/bitCount);
+        }
+            
+        else{
+            ret = readStringSteg(steg,bitCount,enc,2+16/bitCount);
+        }
+           
+        return ret;
+    }
+    
+    //hide a file
+    public static int write(Path topFile,Path bottomFile,int bitCount,String newFileName){
+        if(bitCount!=1 && bitCount!=2)
+            return Steg.ERR_BITCOUNT;
+        BufferedImage image;
+        String fileName = bottomFile.getFileName().toString();
+        
+        Chunker fileChk = new Chunker(Byte.SIZE,bitCount*3);
+        try {
+            image = ImageIO.read(topFile.toFile());
+            fileChk.add(Files.readAllBytes(bottomFile));
+        } catch (IOException e) {
+            Logger.getLogger(Steg.class.getName()).log(Level.SEVERE, null, e);
+            return Steg.ERR_FILEREAD;
+        }
+        
+        Chunker sizeChk = new Chunker(30,bitCount*3);
+        sizeChk.add(fileChk.getSize());
+        Chunker fileNameChk = new Chunker(Byte.SIZE,bitCount*3);
+        Chunker fileNameSizeChk = new Chunker(12,bitCount*3);
+        fileNameChk.add(fileName.getBytes());
+        fileNameSizeChk.add(fileNameChk.getSize());
+        
+        int totalPixReq = 2+ sizeChk.getSize()+ fileNameSizeChk.getSize() + fileChk.getSize()+fileNameChk.getSize()+48/(3*bitCount); //+48/(3*bitCount) for iSteg0
+        if(totalPixReq > image.getHeight()*image.getWidth()) {
+        	return Steg.ERR_LOWIMGSIZE;
+        }
+        
+        int i=init1Meta(image,bitCount,0,0);
+        i=imageWrite(i,image,sizeChk,bitCount);
+        i=imageWrite(i,image,fileNameSizeChk,bitCount);
+        i=imageWrite(i,image,fileChk,bitCount);
+        imageWrite(i,image,fileNameChk,bitCount);
+        
+        return fileWrite(image,newFileName);
+    }
+    
+    //hide a text
+    public static int write(Path topFile,String text,int bitCount,String newFileName){
+        if(bitCount!=1 && bitCount!=2)
+            return Steg.ERR_BITCOUNT;
+        BufferedImage image;
+        Chunker textChk = new Chunker(Byte.SIZE,bitCount*3);
+        try {
+            image = ImageIO.read(topFile.toFile());
+            textChk.add(text.getBytes());
+        } catch (IOException e) {
+            return Steg.ERR_FILEREAD;
+        }
+        
+        Chunker sizeChk = new Chunker(18,bitCount*3);
+        sizeChk.add(textChk.getSize());
+        
+        int i=init1Meta(image,bitCount,1,0);
+
+        int totalPixReq = 2+ textChk.getSize() + sizeChk.getSize()+48/(3*bitCount); //+48/(3*bitCount) for iSteg0
+        if(totalPixReq > image.getHeight()*image.getWidth()) {
+        	return Steg.ERR_LOWIMGSIZE;
+        }
+        
+        i=imageWrite(i,image,sizeChk,bitCount);
+        imageWrite(i,image,textChk,bitCount);
+        
+        return fileWrite(image,newFileName);
+    }
+    
+    
+    //changes pixel value of the image according to chunks and bitCount
+    private static int imageWrite(int begIndex, BufferedImage image, Chunker chunk, int bitCount){
+        OneDimMaker odm = new OneDimMaker(image.getWidth(),image.getHeight());
+        int maxValue = (bitCount==2)?0b111111:0b1111111;
+        int i=begIndex;
+        int dataMaxValue = (bitCount==2)?0b11:0b1;
+        for(;chunk.hasNext();i++){
+            int rgb = image.getRGB(odm.get(i).x, odm.get(i).y);
+            int data = chunk.get();
+            int red = (rgb>>(16+bitCount))<<bitCount | data>>(bitCount*2);
+            int green = (rgb>>(8+bitCount) & maxValue)<<bitCount | data>>bitCount & dataMaxValue;
+            int blue = (rgb>>(bitCount) & maxValue)<<bitCount | data & dataMaxValue;
+            rgb=(((rgb>>24 <<8) | red)<<8 | green)<<8 | blue;
+            
+            image.setRGB(odm.get(i).x, odm.get(i).y, rgb);
+        }
+        return i;
+    }
+
+    //adds metaData like file or text bitCount at 0,0 of image
+    private static int init1Meta(BufferedImage image, int bitCount, int type, int enc) {
+        Chunker chk = new Chunker(Byte.SIZE,bitCount*3);
+        chk.add("iSteg0".getBytes(StandardCharsets.US_ASCII));
+        int rgb0 = image.getRGB(0,0);
+        int red0 = (rgb0>>17)<<1 | (bitCount-1);
+        int green0 = (rgb0>>9 & 0b1111111)<<1 | type;
+        int blue0 = (rgb0>>1 & 0b1111111)<<1 | enc;
+        rgb0=(((rgb0>>24 <<8) | red0)<<8 | green0)<<8 | blue0;
+        image.setRGB(0, 0, rgb0);
+        return imageWrite(2,image,chk,bitCount);
+    }
+
+    private static int fileWrite(BufferedImage image, String newFileName) {  
+        try {
+            ImageIO.write(image,"png", new File(newFileName));
+        } catch (IOException ex) {
+            return Steg.ERR_FILEWRITE; 
+        }
+        return Steg.SUCCESS;
+    }
+
+    private static String[] readFileSteg(BufferedImage steg,int bitCount,int enc,int startIndex) {
+        String ret[] = new String[]{"","","0"};
+        Chunker fileSizeCalc = new Chunker(bitCount*3,30);
+        Chunker nameSizeCalc = new Chunker(bitCount*3,12);
+        Chunker file = new Chunker(bitCount*3,Byte.SIZE);
+        Chunker fileName = new Chunker(bitCount*3,Byte.SIZE);
+        int bitMaxVal = (bitCount==2)? 0b11:0b1;
+        int bitMaxVal3 = (bitCount==2)? 0b111111:0b111;
+        int fileSizeEnd = 10/bitCount+startIndex;
+        int nameSizeEnd = 4/bitCount +fileSizeEnd;
+        OneDimMaker odm = new OneDimMaker(steg.getWidth(),steg.getHeight());
+        for(int i=startIndex;i<fileSizeEnd;i++){
+            int rgb = steg.getRGB(odm.get(i).x, odm.get(i).y);
+            int value= rgb>>16 & bitMaxVal;
+            value = value<<bitCount | (rgb>>8 & bitMaxVal);
+            value = value<<bitCount | (rgb & bitMaxVal);
+            fileSizeCalc.add(value & bitMaxVal3);
+        }
+        for(int i=fileSizeEnd;i<nameSizeEnd;i++){
+            int rgb = steg.getRGB(odm.get(i).x, odm.get(i).y);
+            int value= rgb>>16 & bitMaxVal;
+            value = value<<bitCount | (rgb>>8 & bitMaxVal);
+            value = value<<bitCount | (rgb & bitMaxVal);
+            nameSizeCalc.add(value);
+        }
+        
+        int fileEnd = nameSizeEnd + fileSizeCalc.get();
+        int nameEnd = fileEnd + nameSizeCalc.get();
+        
+        for(int i=nameSizeEnd;i<fileEnd;i++){
+            int rgb = steg.getRGB(odm.get(i).x, odm.get(i).y);
+            int value= rgb>>16 & bitMaxVal;
+            value = value<<bitCount | (rgb>>8 & bitMaxVal);
+            value = value<<bitCount | (rgb & bitMaxVal);
+            file.add(value);
+        }
+        
+        for(int i=fileEnd;i<nameEnd;i++){
+            int rgb = steg.getRGB(odm.get(i).x, odm.get(i).y);
+            int value= rgb>>16 & bitMaxVal;
+            value = value<<bitCount | (rgb>>8 & bitMaxVal);
+            value = value<<bitCount | (rgb & bitMaxVal);
+            fileName.add(value);
+        }
+        try {
+            fos = new FileOutputStream(new String(fileName.getChunkedByteArray()));
+            try {
+                fos.write(file.getChunkedByteArray());
+            } catch (IOException ex) {
+                ret[0] += Steg.ERR_FILEWRITE;
+                Logger.getLogger(Steg.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (FileNotFoundException ex) {
+            ret[0]+=Steg.ERR_FILEWRITE;
+            Logger.getLogger(Steg.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ret[0]+=Steg.SUCCESS;
+        ret[1] = new String(fileName.getChunkedByteArray());
+        return ret;
+    }
+
+    private static String[] readStringSteg(BufferedImage steg,int bitCount,int enc,int startIndex) {
+        String ret[] = new String[]{"","","1"};
+        Chunker textLenCalc = new Chunker(bitCount*3,18);
+        int bitMaxVal = (bitCount==2)? 0b11:0b1;
+        //int bitMaxVal3 = (bitCount==2)? 0b111111:0b111;
+        int textStart = 6/bitCount+startIndex;
+        OneDimMaker odm = new OneDimMaker(steg.getWidth(),steg.getHeight());
+        for(int i=startIndex;i<textStart;i++){
+            int rgb = steg.getRGB(odm.get(i).x, odm.get(i).y);
+            int value= rgb>>16 & bitMaxVal;
+            value = value<<bitCount | (rgb>>8 & bitMaxVal);
+            value = value<<bitCount | (rgb & bitMaxVal);
+            textLenCalc.add(value);
+        }
+        int textEnd = textLenCalc.get() + textStart;
+        Chunker toByte = new Chunker(bitCount*3,Byte.SIZE);
+        for(int i=textStart;i<textEnd;i++){
+            int rgb = steg.getRGB(odm.get(i).x, odm.get(i).y);
+            int value= rgb>>16 & bitMaxVal;
+            value = value<<bitCount | (rgb>>8 & bitMaxVal);
+            value = value<<bitCount | (rgb & bitMaxVal);
+            toByte.add(value);
+        }
+        ret[1] = new String(toByte.getChunkedByteArray());
+        ret[0] += Steg.SUCCESS;
+        return ret;
+    }
+
+   
+}
+
+final class OneDimMaker{
+    //Use for accessing two dimensional array using just one index
+
+    public int totalSize;
+    public int w,h;
+    public OneDimMaker(int w,int h){
+        totalSize = w*h;
+        this.h=h;
+        this.w=w;
+    }
+    
+    public Point get(int i){
+        if(i>=totalSize)
+            return null;
+        int x,y;
+        y = i/w;
+        if(y==0)
+           x=i;
+        else
+            x= i%(w*y);
+        
+        return new Point(x,y);
+    }
+}
